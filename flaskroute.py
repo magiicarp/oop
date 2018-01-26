1a51dc14aed6c74e14d966483746b2cdefcb4dba
Aaron Lam <aaronlammpn@gmail.com>
2018-01-27 00:01:06 +0800

    Updated FlaskRoute with Health Tracker functions.

--- flaskroute.py
+++ flaskroute.py
@@ -3,6 +3,9 @@ from wtforms import Form, StringField, TextAreaField, RadioField, SelectField, S
 from target1 import Target
 import firebase_admin
 from firebase_admin import credentials, db
+from target1 import Target
+#from bmi1 import Bmi
+from User import User
 
 app = Flask(__name__)
 
@@ -24,7 +27,7 @@ def healthyeating():
 
 
 class Plannerform(Form):
-    age = SelectField('What is your age group?', choices = [('', 'Select'), ('TEENAGER', 'Teenager'), ('ADULT', 'Adult'), ('ELDERLY', 'Elderly')],default='')
+    age = SelectField('What is your age group?', choices = [('', 'Select'), ('TEENAGER', 'Teenager (13-19 Years)'), ('ADULT', 'Adult (20-49 Years)'), ('ELDERLY', 'Elderly (above 50)')],default='')
     type = SelectField('What type of exercise would you like to do?', choices = [('', 'Select'), ('AEROBICS', 'Aerobics'), ('STRENGTH', 'Strength'), ('FLEXIBILITY', 'Flexibility')], default ='')
     time = SelectField('Choose the duration of exercise', choices = [('', 'Select'), ('30min', '30 Min'), ('60min', '60 Min'), ('90min', '90 Min')], default='')
 
@@ -122,15 +125,9 @@ def plannerform():
 def routine_1():
     return render_template('routine_1.html')
 
-
-
-
-
-
-
-
-
-
+@app.route('/routine_2')
+def routine_2():
+    return render_template('routine_2.html')
 
 
 @app.route('/events')
@@ -153,17 +150,30 @@ def tracker():
 def teenagerplan():
     return render_template('teenagerplan.html')
 
-@app.route('/teenageraerobics')
-def teenageraerobics():
-    return render_template('teenageraerobics.html')
 
-@app.route('/teenageraerobic30min')
-def teenageraerobic30min():
-    return render_template('teenageraerobic30min.html')
-@app.route('/bmi')
-def bmi():
-    return render_template('bmi.html')
+@app.route('/bmi', methods=['GET','POST'])
+class bmi(Form):
+    weight = StringField('Weight:', [validators.DataRequired()])
+    height = StringField('Height:', [validators.DataRequired()])
 
+def calculate():
+    #follow according to user profile
+    #adduser = request.form[]
+    #user1
+    form = bmi(request.form)
+    if request.method == 'POST' and form.validate():
+        weight = float(form.weight.data)
+        height = float(form.height.data)
+        Bmi = bmi1.Bmi(height, weight)
+        bmi_db = User.child('BMI')
+        bmi_db.update({
+            "height":float(Bmi.get_height()),
+            "weight":float(Bmi.get_weight()),
+            "BMI":str(Bmi.get_bmi())
+        })
+        flash('You have successfully post your BMI','success')
+        return redirect(url_for('bmi'))
+    return render_template('bmiteseting.html')
 @app.route('/viewtarget')
 def viewtarget():
     #print(root.get())
@@ -171,7 +181,7 @@ def viewtarget():
     targets = targets_ref.get()
     for target3 in targets:
         targetpost = targets[target3]
-        target2 = Target(targetpost['goals'], targetpost['category'], targetpost['status'], targetpost['number'])
+        target2 = Target1(targetpost['goals'], targetpost['category'], targetpost['status'], targetpost['number'])
         target2.set_target3(target3)
         target2.get_target3()
         list1.append(target2)
@@ -197,7 +207,7 @@ class RequiredIf(object):
 class Target1(Form):
     goals = StringField('Goals', [validators.Length(min=1, max=150), validators.DataRequired()])
     category = RadioField('Type Of Goals', choices=[('physical', 'Physical'), ('diet', 'Diet'), ('habits', 'Habits')])
-    status = RadioField('Status', choices=[('active', 'Active'),('inactive', 'Inactive'), ('ended', 'Ended'), ('cancelled', 'Cancelled')])
+    status = RadioField('Status', choices=[('active', 'Active'),('onhold', 'On Hold'), ('notstarted', 'Not Started'), ('completed', 'Completed')])
     number = StringField('Number of times daily',[validators.Length(min=1, max=100), validators.DataRequired()])
     submit = SubmitField('Update')
 
@@ -209,7 +219,7 @@ def new():
         category = targetform.category.data
         status = targetform.status.data
         number = targetform.number.data
-        tar = Target(goals, category, status, number)
+        tar = Target1(goals, category, status, number)
         tar_db = root.child('targets')
         tar_db.push({
             'goals': tar.get_goals(),
@@ -231,7 +241,7 @@ def update_target(id):
         category = targetform.category.data
         status = targetform.status.data
         number = targetform.number.data
-        tar = Target(goals, category, status, number)
+        tar = Target1(goals, category, status, number)
         tar_db = root.child('targets/' + id)
         tar_db.set({
             'goals': tar.get_goals(),
@@ -246,7 +256,7 @@ def update_target(id):
         url = 'targets/' + id
         target_s = root.child(url).get()
 
-        target2 = Target(target_s['goals'], target_s['category'], target_s['status'], target_s['number'])
+        target2 = Target1(target_s['goals'], target_s['category'], target_s['status'], target_s['number'])
         target2.set_target3(id)
         targetform.goals.data = target2.get_goals()
         targetform.category.data = target2.get_category()
@@ -257,13 +267,28 @@ def update_target(id):
 
     #return render_template('update_target.html', form=targetform)
 
-@app.route('/delete_target/<string:id>', methods=['POST'])
+@app.route('/delete_targets/<string:id>', methods=['POST'])
 def delete_targets(id):
     tar_db = root.child('targets/' + id)
     tar_db.delete()
     flash('Deleted', 'success')
 
-    return redirect(url_for('viewtargets'))
+    return redirect(url_for('viewtarget'))
+
+
+@app.route('/foodtracker')
+def food():
+    calories = {'Boiled Eggs': 155, 'Fried Eggs': 196, 'Whole Chicken': 1070, 'French Fries': 312,'Celery': 16, 'Broccoli': 34, 'Cabbage': 25, 'Potato': 77, 'Apple': 52, 'Cucumber': 16,
+                'Onion':40,'White Rice(132g, a cup)': 199, 'Chicken': 239, 'Beef': 250}
+
+    count = 0
+    for i in calories:
+        count += 1
+    #if click == True:
+        sum(calories.values())
+    return render_template('foodtracker.html', calories=calories)
+
+
 if __name__ == '__main__':
     app.secret_key = 'secret12'
-    app.run(port=80)
+    app.run(port=80) 
