849747378b28e4fe86fa92028af04149820fbd70
magiicarp <171702L@mymail.nyp.edu.sg>
2018-02-04 19:08:40 +0800

    edit user.py

--- User.py
+++ User.py
@@ -1,11 +1,12 @@
 class User:
-    def __init__(self,name,username,age,email,password):
+    def __init__(self,name,username,email,birthday,password):
         self.__name = name
         self.__username = username
-        self.__age = age
         self.__email = email
+        self.__birthday = birthday
+        #self.__gender = gender
         self.__password = password
-
+        #self.about = ''
 #NAME
     def get_name(self):
         return self.__name
@@ -16,21 +17,28 @@ class User:
         return self.__username
     def set_username(self,username):
         self.__username = username
-
 #PASSWORD
     def get_password(self):
         return self.__password
     def set_password(self,password):
         self.__password = password
-#AGE
-    def get_age(self):
-        return self.__age
-    def set_birthdate(self,age):
-        self.__age = age
 #EMAIL
     def get_email(self):
         return self.__email
     def set_email(self,email):
         self.__email = email
-
-
+#ABOUT
+#    def get_about(self):
+#        return self.__about
+#    def set_about(self,about):
+#        self.__about = about
+#BIRTHDAY
+    def get_birthday(self):
+        return self.__birthday
+    def set_birthday(self,birthday):
+        self.__birthday = birthday
+#GENDER
+    #def get_gender(self):
+    #    return self.__gender
+    #def set_gender(self,birthday):
+    #    self.__gender = birthday
