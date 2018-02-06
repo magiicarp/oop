37d561ada32bc70b45cc20c3099cbd1e855b5ed5
magiicarp <171702L@mymail.nyp.edu.sg>
2018-02-06 11:46:01 +0800

    Update

--- User.py
+++ User.py
@@ -1,12 +1,12 @@
 class User:
-    def __init__(self,name,username,email,birthday,password):
+    def __init__(self,name,username,email,password,region,birthday,gender):
         self.__name = name
         self.__username = username
         self.__email = email
-        self.__birthday = birthday
-        #self.__gender = gender
         self.__password = password
-        #self.about = ''
+        self.__region = region
+        self.__birthday = birthday
+        self.__gender = gender
 #NAME
     def get_name(self):
         return self.__name
@@ -27,18 +27,19 @@ class User:
         return self.__email
     def set_email(self,email):
         self.__email = email
-#ABOUT
-#    def get_about(self):
-#        return self.__about
-#    def set_about(self,about):
-#        self.__about = about
+
 #BIRTHDAY
     def get_birthday(self):
         return self.__birthday
     def set_birthday(self,birthday):
         self.__birthday = birthday
 #GENDER
-    #def get_gender(self):
-    #    return self.__gender
-    #def set_gender(self,birthday):
-    #    self.__gender = birthday
+    def get_gender(self):
+        return self.__gender
+    def set_gender(self,gender):
+        self.__gender = gender
+#GENDER
+    def get_region(self):
+        return self.__region
+    def set_region(self,region):
+        self.__region = region
